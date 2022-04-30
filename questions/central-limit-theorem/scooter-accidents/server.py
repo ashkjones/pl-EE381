acb63dc0ba15567f6251c3f305ccce8981354570
ashkjones <ashkjones@gmail.com>
2022-04-30 09:55:08 -0700

    created vespa question

--- questions/central-limit-theorem/scooter-accidents/server.py
+++ questions/central-limit-theorem/scooter-accidents/server.py
@@ -1,24 +1,26 @@
 import random
+import math
+import numpy as np
+import scipy.stats as stats
 
 def generate(data):
+    choices = []
+    average = 9
+    x = 11
+    root = math.sqrt(average)
 
-    # Sample a random decimal number in the tenths place between 1.1 and 5
-    a = round(random.uniform(1.1, 5), 1)
-
-    # Sample an integer number between 6 and 11
-    b = random.randint(6, 11)
-
-    # Put these two decimal numbers into data['params']
-    data['params']['a'] = a
-    data['params']['b'] = b
-
-    # Compute the product of these two numbers
-    c = a * b
-
-    # Put the product into data['params']
-    data['params']['correct_answer'] = c
+    # adding inclusive answers
+    # correct_z = np.array([(x - 0.5) / root])
+    # table_value = stats.zscore(correct_z)
+    choices.append({"text": 0.6915, "answer": False, "feedback": "This is the probability there is less than 11."})
+    choices.append({"text": 0.3085, "answer": True})
     
-    # Generate three distractors by changing the operation
-    data['params']['wrong_answer1'] = (a - 1) * b
-    data['params']['wrong_answer2'] = b - a
-    data['params']['wrong_answer3'] = (a + 1) * b
+    # adding exclusive answers
+    # wrong_z = np.array([x / root])
+    # table_value = stats.zscore(wrong_z)
+    choices.append({"text": 0.7486, "answer": False, "feedback": "Less than 11 inclusive"})
+    choices.append({"text": 0.2514, "answer": False, "feedback": "Less than 11 inclusive"})
+
+    data["params"]["average"] = average
+    data["params"]["x"] = x
+    data["params"]["choices"] = choices
